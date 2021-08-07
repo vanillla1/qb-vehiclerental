d5de3816f249038a586451068468351419b723c0
vanillaa <80897991+vanillla1@users.noreply.github.com>
2021-08-07 14:24:02 +0300

    add new script rental

---
+++ config.lua
@@ -0,0 +1,29 @@
+Config.RentelVehicles = {
+	['sultan'] = { ['sultan'] = 'sultan', ['label'] = 'Sultan Car', ['price'] = 100},
+	['t20'] = { ['model'] = 't20', ['label'] = 'T20 Car', ['price'] = 100},
+  ['adder'] = { ['model'] = 'adder', ['label'] = 'Adder Car', ['price'] = 250},
+}
+
+Config.RentelLocations = {
+    ['Courthouse Paystation'] = {
+        ['coords'] = vector4(129.93887, -898.5326, 30.148599, 166.04177)
+    },
+    ['Train Station'] = {
+        ['coords'] = vector4(-213.4004, -1003.342, 29.144016, 345.36584)
+    },
+    ['Bus Station'] = {
+        ['coords'] = vector4(416.98699, -641.6024, 28.500173, 90.011344)
+    },    
+    ['Morningwood Blvd'] = {
+        ['coords'] = vector4(-1274.631, -419.1656, 34.215377, 209.4456)
+    },    
+    ['South Rockford Drive'] = {
+        ['coords'] = vector4(-682.9262, -1112.928, 14.525076, 37.729667)
+    },    
+    ['Tinsel Towers Street'] = {
+        ['coords'] = vector4(-716.9338, -58.31439, 37.472839, 297.83691)
+    },
+    ['magazin ul de masini nu e gata'] = {
+        ['coords'] = vector4(252.51887, -635.4688, 40.480518, 182.19883)
+    }     
+}
